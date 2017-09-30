07903f9d4e2d2303782dc1fa12d1bd04bb81452b
nealhartley <neal.w.hartley@gmail.com>
2017-10-01 02:33:35 +1300

    Added delete function, going to bed.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -37,6 +37,22 @@ export class HeroService {
       .catch(this.handleError);
   }
 
+  create(name: string):  Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError);
+  }
+
+  delete(id: number): Promise<void>{
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
   private handleError(error: any): Promise<any> {
     console.error('An error occured', error); // for demo only
     return Promise.reject(error.message || error);
